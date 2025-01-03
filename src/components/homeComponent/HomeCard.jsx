4cdf2ba03caa88004da0f00ec095aa6664e8a9ea
tanvir-alam-sk <tanviralamsk47@gmail.com>
2025-01-03 17:34:54 +0600

    readme

--- src/components/homeComponent/HomeCard.jsx
+++ src/components/homeComponent/HomeCard.jsx
@@ -3,11 +3,11 @@
 import { FaRegHeart, FaHeart } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 export default function HomeCard({ photo, handelFavorite }) {
-  const { url, title } = photo;
+  const { url, title, id } = photo;
 
-  const dispatch=useDispatch()
-  const favorite=useSelector((state)=>state.likes)
-
+  const dispatch = useDispatch();
+  const favorite = useSelector((state) => state.likes);
+  const remain = favorite.find((postId) => postId.id == id);
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
       <img className="w-full" src={url} alt="Sunset in the mountains" />
@@ -15,10 +15,11 @@ export default function HomeCard({ photo, handelFavorite }) {
         <div className="font-bold text-xl mb-2">{title}</div>
       </div>
       <div className="px-6 pt-4 pb-2">
-        <button className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-          <FaRegHeart onClick={() => handelFavorite(photo)} />
-          {/* <FaHeart onClick={() => handelFavorite(photo)} /> */}
-        </button>
+        {!remain && (
+          <button className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            <FaRegHeart onClick={() => handelFavorite(photo)} />
+          </button>
+        )}
       </div>
     </div>
   );
